008f46c793ed74a205b4cb7f5e47b8b31e8a38f8
Lauren Kessell <lkessell1@gmail.com>
2021-08-30 17:30:30 -0400

    Add route and render SkillsContainer on correct path

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import { SkillsContainer } from "./SkillsContainer";
 
 export const App = () => {
   return (
-    <h1>Star Wars RPG Companion</h1>
+    <>
+      <h1>Star Wars RPG Companion</h1>
+      <Route path="/skills">
+        <SkillsContainer />
+      </Route>
+    </>
   );
 }
 
