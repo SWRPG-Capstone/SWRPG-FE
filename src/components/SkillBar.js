69334581746b65dba5fca723706aff48a8ec9b40
Lauren Kessell <lkessell1@gmail.com>
2021-08-30 17:12:32 -0400

    Add function to format skill names

--- src/components/SkillBar.js
+++ src/components/SkillBar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { formatName } from "../utilities";
 
 export const SkillBar = ({ skillName, ranks }) => {
   return (
     <div>
-      <label for={skillName}>{skillName}</label>
+      <label for={skillName}>{formatName(skillName)}</label>
       <meter id={skillName} value={ranks} min="0" max="5"></meter>
     </div>
   );
