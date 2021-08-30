8beffcf9d28d0f221d03815f583b39ebd0c238df
Lauren Kessell <lkessell1@gmail.com>
2021-08-30 16:59:49 -0400

    Set up skills section, render skill bars from sample data

---
+++ src/components/SkillsContainer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { SkillBar } from "./SkillBar";
+import { sampleData } from "../mock-data";
+
+export const SkillsContainer = () => {
+  const skills = sampleData.data.attributes.character.skills;
+  
+  const skillBars = Object.keys(skills).map(skill => {
+    const ranks = sampleData.data.attributes.character.skills[skill];
+    return <SkillBar skillName={skill} ranks={ranks}/>
+  });
+  
+  return (
+    <section>
+      <h2>Skills</h2>
+      <h2>Ranks</h2>
+      {skillBars}
+    </section>
+  );
+}
