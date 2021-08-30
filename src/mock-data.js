5d619d0b27e9141ba1849fad80583889a2cdd2b1
Lauren Kessell <lkessell1@gmail.com>
2021-08-30 15:18:15 -0400

    Add mock data file

---
+++ src/mock-data.js
@@ -0,0 +1,60 @@
+{
+    "data": {
+        "id": "1",
+        "type": "user",
+        "attributes": {
+            "name": "Andrew Vallejo",
+            "email": "andrew.a.vallejo@gmail.com",
+            "password": "alligator1",    
+            "character":  {
+                "name":  "Vaergh Somis",
+                "species":  "Kaleesh",
+                "career":  "Bounty Hunter"
+                "specialization": "Martial Artist",
+                "characteristics": {
+                    "agility": 2,
+                    "brawn": 4,
+                    "presence": 4,
+                    "intellect": 3,
+                    "willpower": 3,
+                    "cunning": 2, 
+                }
+                "skills": {
+                    "astrogation": 0
+                    "athletics": 2,
+                    "charm": 4,
+                    "coercion": 0,
+                    "computers": 0,
+                    "cool": 1,
+                    "coordination": 1,
+                    "deception": 0,
+                    "discipline": 0,
+                    "leadership": 0,
+                    "mechanics": 1,
+                    "medicine": 5,
+                    "negotiation": 3,
+                    "perception": 2,
+                    "piloting_planetary": 0,
+                    "piloting_space": 0,
+                    "resilience": 4,
+                    "skulduggery": 1,
+                    "stealth": 1,
+                    "streetwise": 0,
+                    "survival": 3, 
+                    "vigilance": 2,
+                    "brawl": 4,
+                    "gunnery": 1,
+                    "melee": 5,
+                    "ranged_light": 0,
+                    "ranged_heavy": 2,
+                    "core_worlds": 1,
+                    "education": 0,
+                    "lore": 0,
+                    "outer_rim": 1,
+                    "underworld": 2,
+                    "xenology": 0,
+                }
+            }
+        }
+    }
+}
