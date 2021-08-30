63bb1dded64438f051734740ec15523bf5900c59
Lauren Kessell <lkessell1@gmail.com>
2021-08-30 17:30:50 -0400

    Fix console errors

--- src/components/SkillBar.js
+++ src/components/SkillBar.js
@@ -4,7 +4,7 @@ import { formatName } from "../utilities";
 export const SkillBar = ({ skillName, ranks }) => {
   return (
     <div>
-      <label for={skillName}>{formatName(skillName)}</label>
+      <label htmlFor={skillName}>{formatName(skillName)}</label>
       <meter id={skillName} value={ranks} min="0" max="5"></meter>
     </div>
   );
