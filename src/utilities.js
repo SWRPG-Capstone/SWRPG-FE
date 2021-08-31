7d692fc56fd29deaabc818c9efc2f3a7a5b74663
Lauren Kessell <lkessell1@gmail.com>
2021-08-31 15:45:35 -0400

    Adjust font formatting

--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,3 @@
 export const formatName = (string) => {
-  const split = string.split("_");
-  return split.map(word => word[0].toUpperCase() + word.substr(1)).join(" ");
+  return string.split("_").join(" ");
 }
