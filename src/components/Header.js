afc82c89c87496a7284b014b32102e791f48b638
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-31 14:38:39 -0600

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useLocation } from "react-router";
+
+export const Header = () => {
+  const location = useLocation().pathname
+  const pageTitle = location.slice(1)
+
+  return (
+    <header className='main-header'>
+      <h1 className='title'>{pageTitle}</h1>
+    </header>
+  )
+} 
