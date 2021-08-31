b52712b97f64a5e9d2e9aeb9c708db1f88e8a2a9
Lauren Kessell <lkessell1@gmail.com>
2021-08-31 19:12:58 -0400

    Add character page path

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { SkillsPage } from "./SkillsPage";
+import { CharacterPage } from "./CharacterPage";
 
 export const App = () => {
   return (
     <>
       <h1>Star Wars RPG Companion</h1>
+      <Route path="/character">
+        <CharacterPage />
+      </Route>
       <Route path="/skills">
         <SkillsPage />
       </Route>
