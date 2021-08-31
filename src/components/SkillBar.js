591197cf05f5639ecf68d627ac194cdea9dfb8f5
Lauren Kessell <lkessell1@gmail.com>
2021-08-31 13:36:09 -0400

    Add styling for skill/ranks bar

--- src/components/SkillBar.js
+++ src/components/SkillBar.js
@@ -3,9 +3,9 @@ import { formatName } from "../utilities";
 
 export const SkillBar = ({ skillName, ranks }) => {
   return (
-    <div>
+    <div className="skill-element">
       <label htmlFor={skillName}>{formatName(skillName)}</label>
-      <meter id={skillName} value={ranks} min="0" max="5"></meter>
+      <progress className="skill-meter" htmlFor={skillName} id={skillName} value={ranks} min="0" max="5" />
     </div>
   );
 }
