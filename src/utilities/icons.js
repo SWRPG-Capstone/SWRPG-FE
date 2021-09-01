74e6dca155cd4e8ab2249c68c6bfb5ff0f518196
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-01 17:47:00 -0600

    Add util file to easily store image imports

---
+++ src/utilities/icons.js
@@ -0,0 +1,13 @@
+import homeIcon from '../assets/images/journal.png';
+import characterIcon from '../assets/images/person.png';
+import rollIcon from '../assets/images/roll.png';
+import skillsIcon from '../assets/images/dual-sabers.png';
+import logoutIcon from '../assets/images/deathstar.png';
+
+export const icons = {
+  home: homeIcon,
+  character: characterIcon,
+  roll: rollIcon,
+  skills: skillsIcon,
+  logout: logoutIcon
+}
