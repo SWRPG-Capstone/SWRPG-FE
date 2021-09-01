8b279d67d8680db9e8abc48994ff6669a872400c
Lauren Kessell <lkessell1@gmail.com>
2021-09-01 17:22:52 -0400

    Add Apollo client and wrap components with Apollo Provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './styles/styles.scss';
 
+const client = new ApolloClient({
+  uri: 'https://swrpg-be.herokuapp.com/graphql',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
- <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+  <Router>
+    <ApolloProvider client={client}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ApolloProvider>
   </Router>,
   document.getElementById('root')
 );
