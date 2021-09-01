894b5cfcfa74f0e2824dc375ab036eac3a384a89
Lauren Kessell <lkessell1@gmail.com>
2021-09-01 17:23:32 -0400

    Add GraphQL query, render character details from server data

--- src/components/CharacterPage.js
+++ src/components/CharacterPage.js
@@ -1,23 +1,41 @@
 import React from "react";
 import { InfoField } from "./InfoField";
 import { Characteristics } from "./Characteristics";
+import { useQuery, gql } from "@apollo/client";
 import { sampleData } from "../mock-data";
 
+const CHARACTER = gql`
+  query getCharacter {
+    character(id: 1){
+      id
+      name
+      species
+      specialization
+      career
+    } 
+  }
+`
+
 export const CharacterPage = () => {
-  const character = sampleData.data.attributes.character;
+  const characterSamp = sampleData.data.attributes.character;
+  const { loading, error, data } = useQuery(CHARACTER);
+
+  if (loading) return 'Loading your data...';
+  if (error) return `Error! ${error.message}`;
+
   const characterInfo = (
     <>
-    <InfoField heading={Object.keys(character)[0]} info={character.name}/>
-    <InfoField heading={Object.keys(character)[1]} info={character.species}/>
-    <InfoField heading={Object.keys(character)[2]} info={character.career}/>
-    <InfoField heading={Object.keys(character)[3]} info={character.specialization}/>
+    <InfoField heading={Object.keys(data.character)[3]} info={data.character.name}/>
+    <InfoField heading={Object.keys(data.character)[5]} info={data.character.species}/>
+    <InfoField heading={Object.keys(data.character)[1]} info={data.character.career}/>
+    <InfoField heading={Object.keys(data.character)[4]} info={data.character.specialization}/>
     </>
   );
   return (
     <section>
       {characterInfo}
       <h2>Characteristics</h2>
-      <Characteristics characteristics={character.characteristics}/>
+      <Characteristics characteristics={characterSamp.characteristics}/>
     </section>
   )
 }
