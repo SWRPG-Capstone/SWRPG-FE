691d3e5d549b120edb492a8abcdc8dc90fc274ad
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-01 17:48:58 -0600

    Add Orb components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Orb } from './Orb';
 
 export const NavBar = () => {
 
   return (
     <section className='nav-bar'>
-
+      <Orb />
+      <Orb />
+      <Orb />
+      <Orb />
+      <Orb />
     </section>
   )
 }
