d417fe634e0e2d074a6ac58bceb06bab3601f3dc
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-01 18:39:35 -0600

    Add icon props to pass into Orb

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Orb } from './Orb';
+import { icons } from '../utilities/icons';
 
 export const NavBar = () => {
+  const { home, character, roll, skills, logout } = icons
 
   return (
-    <section className='nav-bar'>
-      <Orb />
-      <Orb />
-      <Orb />
-      <Orb />
-      <Orb />
-    </section>
+    <>
+      <div className='backdrop'/>
+      <section className='nav-bar'>
+        <Orb icon={home} />
+        <Orb icon={character} />
+        <Orb icon={roll} />
+        <Orb icon={skills} />
+        <Orb icon={logout} />
+      </section>
+
+    </>
   )
 }
