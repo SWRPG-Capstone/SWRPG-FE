7ffcc4f8ef567d1c8056f83b0d229306a079c916
Lauren Kessell <lkessell1@gmail.com>
2021-09-02 19:37:33 -0400

    Updated components to use new props

--- src/components/Characteristics.js
+++ src/components/Characteristics.js
@@ -1,10 +1,14 @@
 import { Characteristic } from "./Characteristic";
 
 export const Characteristics = ({ characteristics }) => {
-  const characteristicBubbles = Object.keys(characteristics).map(characteristic => {
-    const score = characteristics[characteristic];
-    return <Characteristic key={characteristic} characteristic={characteristic} score={score} />
-  });
+  const characteristicBubbles = [
+    <Characteristic key='brawn' characteristic='brawn' score={characteristics.brawn} />,
+    <Characteristic key='agility' characteristic='agility' score={characteristics.agility} />,
+    <Characteristic key='cunning' characteristic='cunning' score={characteristics.cunning} />,
+    <Characteristic key='intellect' characteristic='intellect' score={characteristics.intellect} />,
+    <Characteristic key='willpower' characteristic='willpower' score={characteristics.willpower} />,
+    <Characteristic key='presence' characteristic='presence' score={characteristics.charPresence} />,
+  ];
   return (
     <>
     <div className="top-bottom-bubbles">
