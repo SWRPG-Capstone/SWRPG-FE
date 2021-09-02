1742f2fa8a298e7555b054134a7da86d52dae345
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-02 11:55:57 -0600

    Update attributes to work with props from navbar

--- src/components/Orb.js
+++ src/components/Orb.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export const Orb = ({ icon }) => {
-  const iconName = icon.toString()
+export const Orb = ({ pathway, icon, size }) => {
+  const file = icon.substr(14) 
+  const iconName = file.substr(0, file.indexOf('.'))
+
   return (
-    <NavLink className='button' to={`/${iconName}`} >
-      <img className='icon' src={iconName} alt={`${iconName}.`} />
-      <div className='overlay front' />
-      <div className='overlay back' />
+    <NavLink className={`button  ${size}`} to={pathway} >
+      <img className={`icon ${iconName}`}  src={icon} alt={`${iconName} icon.`} />
+      <div className={`orb ${size}`} />
     </NavLink>
   )
 }
