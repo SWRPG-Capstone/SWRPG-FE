051cf8e4d6da9f53e6d6e17f43046b74cd672dc8
Lauren Kessell <lkessell1@gmail.com>
2021-09-02 21:06:54 -0400

    Add query for skills, render skill bars using queried data

--- src/components/SkillsPage.js
+++ src/components/SkillsPage.js
@@ -1,12 +1,58 @@
 import React from "react";
 import { SkillBar } from "./SkillBar";
-import { sampleData } from "../mock-data";
+import { useQuery, gql } from "@apollo/client";
+
+const SKILLS = gql`
+  query getSkills {
+    character(id: 1) {
+      skills {
+        astrogation
+        athletics
+        brawl
+        charm
+        coercion
+        computers
+        cool
+        coordination
+        deception
+        discipline
+        education
+        gunnery
+        leadership
+        lore
+        mechanics
+        melee
+        negotiation
+        outerRim
+        perception
+        piloting
+        pilotingSpace
+        rangedHeavy
+        rangedLight
+        resilience
+        skulduggery
+        stealth
+        streetWise
+        survival
+        underworld
+        vigilance
+        xenology
+      }
+    }
+  }
+`
 
 export const SkillsPage = () => {
-  const skills = sampleData.data.attributes.character.skills;
-  
-  const skillBars = Object.keys(skills).map(skill => {
-    const ranks = sampleData.data.attributes.character.skills[skill];
+  const { loading, error, data } = useQuery(SKILLS);
+
+  if (loading) return 'Loading your data...';
+  if (error) return `Error! ${error.message}`;
+
+  const skills = Object.keys(data.character.skills[0]);
+  skills.shift();
+
+  const skillBars = skills.map(skill => {
+    const ranks = data.character.skills[0][skill];
     return <SkillBar key={skill} skill={skill} ranks={ranks}/>
   });
 
