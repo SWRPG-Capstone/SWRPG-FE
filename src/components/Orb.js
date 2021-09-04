ee2ea40aac5065cc94af805998e41e02ea1f709c
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-03 21:27:33 -0600

    Add logic to hide buttons for better UX experience

--- src/components/Orb.js
+++ src/components/Orb.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { UserContext } from '../utilities/UserContext'
 
 export const Orb = ({ pathway, icon, size }) => {
+  const { state: { isAuthorize }  } = useContext(UserContext)
   const file = icon.substr(14) 
   const iconName = file.substr(0, file.indexOf('.'))
 
   return (
-    <NavLink className={`button  ${size}`} to={pathway} >
+    <>
+    {/* Remove the !-bang operator to disable the hide feature for developement */}
+    <NavLink className={`button ${size} ${!isAuthorize && 'disabled'}`} to={pathway} >
       <img className={`icon ${iconName} ${size}`}  src={icon} alt={`${iconName} icon.`} />
       <div className={`orb ${size}`} />
     </NavLink>
+    </>
   )
 }
 
