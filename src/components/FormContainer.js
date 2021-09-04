d5f467dc104e731b59f3cca840efb856ae20c2e2
Lauren Kessell <lkessell1@gmail.com>
2021-09-04 15:53:09 -0400

    Set up parent container for form pages

---
+++ src/components/FormContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FormSkills } from "./FormSkills";
+import { FormCharacteristics } from "./FormCharacteristics";
+import { FormCharDetails } from "./FormCharDetails";
+
+export const FormContainer = () => {
+  return (
+    <section>
+      <FormCharDetails />
+      {/* <FormCharacteristics /> */}
+      {/* <FormSkills /> */}
+    </section>
+  )
+}
