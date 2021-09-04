3a04b60610e473ff0b6a2993b4d6091dbf194371
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-03 21:26:27 -0600

    Add global context for authorization

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { Route } from "react-router-dom";
 import { Header } from './Header'
 import { SkillsPage } from "./SkillsPage";
 import { CharacterPage } from "./CharacterPage";
 import { NavBar } from "./NavBar";
+import { UserContext } from "../utilities/UserContext";
+import { reducer } from "../utilities/reducer";
+
+const initialState = {
+  isAuthorize: false
+}
 
 export const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  
   return (
-    <main>
-      <Header />
-      <Route path="/character">
-        <CharacterPage />
-      </Route>
-      <Route path="/skills">
-        <SkillsPage />
-      </Route>
-      <NavBar />
-    </main>
-  );
+    <UserContext.Provider value={{ state, dispatch }} >      
+      <main>
+        <Header />
+          <Route path="/character">
+            <CharacterPage />
+          </Route>
+          <Route path="/skills">
+            <SkillsPage />
+          </Route>
+        <NavBar />
+      </main>
+    </UserContext.Provider>    
+  )
 }
+
 
