ad68406be269cf60b9d52b1d0b9966b4da3023ae
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-05 17:53:27 -0600

    Refactor code to have a state for count

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,16 +1,23 @@
-import react from "react"
+import { useState } from "react"
 
-export const Die = ({ die, amount }) => {
+export const Die = ({ die }) => {
+	const [count, setCount] = useState(0)
+
+	const handleClick = (e, sign) => {
+		e.preventDefault()
+		sign === 'plus' ? setCount(count + 1) : setCount(count - 1)
+	}
+
 	return (
 		<article className='die-field'>
- 			<h4 className='die-name'>Profeciency</h4>
+ 			<h4 className='die-name'>{die}</h4>
 			<aside className='dice-scale'>
 			<div className='scale down'>
-					<p className='sign minus'>-</p>
+					<p className='sign minus' onClick={(e) => handleClick(e, 'minus')}>-</p>
 				</div>
-				<h4 className='dice-amount'>3</h4>
+				<h4 className='dice-amount'>{count}</h4>
 				<div className='scale'>
-					<p className='sign plus'>+</p>
+					<p className='sign plus' onClick={(e) => handleClick(e, 'plus')}>+</p>
 				</div>
 			</aside>
 		</article>
