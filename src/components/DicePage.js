419d56967f55233b8108b597ade907fa28d2dcb9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-05 17:51:22 -0600

    Add dice import

--- src/components/DicePage.js
+++ src/components/DicePage.js
@@ -1,13 +1,17 @@
-import react from "react"
 import { Die } from "./Die"
+import { dice } from "../utilities/dice"
 
 export const DicePage = () => {
+	const diceType = dice.map(die => <Die die={die} />)
+
 	return (
 		<section className='dice-sheet'>
 			<h2>Dice Log</h2>
 			<h3>Dice</h3>
 			<h3>Amount</h3>
-			<Die/>
+			<div className='dice-field'>
+				{diceType}
+			</div>
 		</section>
 	)
 }
