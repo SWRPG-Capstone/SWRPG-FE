2d95eee48c6a5bd88f064e73f2c4ce6a20780010
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-05 11:23:51 -0600

    Update class names

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,11 +1,17 @@
+import react from "react"
+
 export const Die = ({ die, amount }) => {
 	return (
 		<article className='die-field'>
- 			<h4 className='die-name'>{die}</h4>
+ 			<h4 className='die-name'>Profeciency</h4>
 			<aside className='dice-scale'>
-				<div className='scale down'/>
-				<h4 className='dice'>{amount}</h4>
-				<div className='scale up'/>
+			<div className='scale down'>
+					<p className='sign minus'>-</p>
+				</div>
+				<h4 className='dice-amount'>3</h4>
+				<div className='scale'>
+					<p className='sign plus'>+</p>
+				</div>
 			</aside>
 		</article>
 	)
