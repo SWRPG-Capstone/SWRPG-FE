8c8bc33175c0601f38002e4d26171fefddcedd86
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-05 17:50:37 -0600

    Add a className to article wrapper

--- src/components/Characteristic.js
+++ src/components/Characteristic.js
@@ -1,6 +1,6 @@
 export const Characteristic = ({ characteristic, score }) => {
   return (
-    <article>
+    <article className='bubble-wrapper'>
       <h3 className="score-bubble">{score}</h3>
       <p className="characteristic-title">{characteristic}</p>
     </article>
