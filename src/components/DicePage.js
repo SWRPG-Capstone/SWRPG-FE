9ea43400288ed02691d0df1e403d770b243a42cc
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-05 09:51:11 -0600

    Add basic JSX for DicePage

--- src/components/DicePage.js
+++ src/components/DicePage.js
@@ -1,5 +1,11 @@
 import react from "react"
 
 export const DicePage = () => {
-	
+	return (
+		<section className='dice-sheet'>
+			<h2>Dice Log</h2>
+			<h3>Dice</h3>
+			<h3>Amount</h3>
+		</section>
+	)
 }
