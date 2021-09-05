2a2d9f88e35fb302e67221fbe017a99152c8cd40
Lauren Kessell <lkessell1@gmail.com>
2021-09-05 17:13:52 -0400

    Render a link for each character, query character data by id on click

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+
+const ALL_CHARACTERS = gql`
+  query getAllCharacters {
+    user(id: 1) {
+      username
+      characters {
+        id
+        name
+      }
+    }
+  }
+`;
 
 export const HomePage = ({ currentChar, setCurrentChar }) => {
+  const { loading, error, data } = useQuery(ALL_CHARACTERS);
+
+  if (loading) return 'Loading your data...';
+  if (error) return `Error! ${error.message}`;
+
+  const handleClick = (e) => {
+    setCurrentChar(e.target.id);
+  }
+
+  const charOptions = data.user.characters.map(character => {
+    return (
+      <Link to='/character' className='home-link' key={character.id} id={parseInt(character.id)} onClick={handleClick}>{character.name}</Link>
+    )
+  });
+
   return (
-    <h1>Home</h1>
+    <>
+    {charOptions}
+    <Link to='/create' className='home-link'>Create a New Character</Link>
+    </>
   )
 }
