7329e7619bf3bc1f08d1019daf4f7ea0cc3daa55
Lauren Kessell <lkessell1@gmail.com>
2021-09-07 16:56:11 -0400

    Add state for skill update modal

--- src/components/SkillBar.js
+++ src/components/SkillBar.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { formatName } from "../utilities/utilities";
+import { UpdateSkillPrompt } from "./UpdateSkillPrompt";
 
-export const SkillBar = ({ skill, ranks }) => {
+export const SkillBar = ({ skill, skillID, ranks }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleClick = (e) => {
+    console.log(e.target.id, e.target.value)
+    console.log("skillID: " + skillID)
+  }
+
+  const toggleModal = () => setModalOpen(!modalOpen);
+
+  const skillModal = modalOpen && <UpdateSkillPrompt skill={skill} skillID={skillID} ranks={ranks} isOpen={modalOpen} closeModal={toggleModal} />;
+
   return (
     <div className="skill-element">
       <label htmlFor={skill}>{formatName(skill)}</label>
-      <progress className="skill-meter" htmlFor={skill} id={skill} value={ranks} min="0" max="5" />
+      <progress className="skill-meter" htmlFor={skill} id={skill} value={ranks} min="0" max="5" onClick={toggleModal} />
+      {skillModal}
     </div>
   );
 }
