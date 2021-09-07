23633eddadce1b853bf318ba205f2e2e87e51ab2
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-07 13:45:54 -0600

    Add key to die and wrap headings

--- src/components/DicePage.js
+++ src/components/DicePage.js
@@ -2,15 +2,17 @@ import { Die } from "./Die"
 import { dice } from "../utilities/dice"
 
 export const DicePage = () => {
-	const diceType = dice.map(die => <Die die={die} />)
+	const diceType = dice.map(die => <Die key={die} die={die} />)
 
 	return (
 		<section className='dice-sheet'>
 			<h2>Dice Log</h2>
-			<h3>Dice</h3>
-			<h3>Amount</h3>
 			<div className='dice-field'>
-				{diceType}
+				<div className="dice-headings">
+					<h3 className='heading'>Dice</h3>
+					<h3 className='heading'>Amount</h3>
+				</div>
+					{diceType}
 			</div>
 		</section>
 	)
