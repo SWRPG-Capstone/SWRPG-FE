4b84cb7642203ebb0d0eb7f67e711869fc4e8820
Lauren Kessell <lkessell1@gmail.com>
2021-09-07 18:48:39 -0400

    Remove handleClick

--- src/components/SkillBar.js
+++ src/components/SkillBar.js
@@ -5,11 +5,6 @@ import { UpdateSkillPrompt } from "./UpdateSkillPrompt";
 export const SkillBar = ({ skill, skillID, ranks }) => {
   const [modalOpen, setModalOpen] = useState(false);
 
-  const handleClick = (e) => {
-    console.log(e.target.id, e.target.value)
-    console.log("skillID: " + skillID)
-  }
-
   const toggleModal = () => setModalOpen(!modalOpen);
 
   const skillModal = modalOpen && <UpdateSkillPrompt skill={skill} skillID={skillID} ranks={ranks} isOpen={modalOpen} closeModal={toggleModal} />;
