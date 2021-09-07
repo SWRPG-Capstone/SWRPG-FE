aed36ba0c060afab473ec5d4381d6772bdacf19a
Lauren Kessell <lkessell1@gmail.com>
2021-09-07 18:38:58 -0400

    Add mutation to increase skill ranks

--- src/components/UpdateSkillPrompt.js
+++ src/components/UpdateSkillPrompt.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useMutation, gql } from '@apollo/client';
 import { formatName } from '../utilities/utilities';
+import { UserContext } from "../utilities/UserContext";
 
 export const UpdateSkillPrompt = ({ skill, skillID, ranks, isOpen, closeModal }) => {
 
-  const addButton = ranks < 5 && <button>Add Rank</button>;
+  const MUTATE_SKILL = gql`
+    mutation mutateSkill ($id: Int!, $characterID: Int!, $ranks: Int!){
+      updateSkill(input: {
+        characterId: $characterID,
+        ${skill}: $ranks,
+        id: $id
+      }) {
+        ${skill}
+      }
+    }
+  `;
+
+  const userContext = useContext(UserContext);
+  console.log(userContext.state.currentChar)
+
+  const mutationVars = {
+    id: parseInt(skillID),
+    characterID: userContext.state.currentChar,
+    ranks: (ranks + 1)
+  }
+
+  const [mutateSkill] = useMutation(MUTATE_SKILL, {
+    variables: mutationVars
+  });
+
+  const addButton = ranks < 5 && <button onClick={() => mutateSkill()}>Add Rank</button>;
   const removeButton = ranks > 0 && <button>Remove Rank</button>;
 
   return (
