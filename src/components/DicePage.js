dc92f5169496e88867273ab124412a81698d14d2
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-07 16:39:48 -0600

    Refactor so that die have more data

--- src/components/DicePage.js
+++ src/components/DicePage.js
@@ -1,8 +1,34 @@
 import { Die } from "./Die"
 import { dice } from "../utilities/dice"
+import { useReducer } from "react";
+import { diceReducer } from "../utilities/diceReducer";
+
+const initialState = {
+	force: 0,
+	ability: 0,
+	profecieny: 0,
+	boost: 0,
+	difficulty: 0,
+	challenge: 0,
+	setback: 0
+}
 
 export const DicePage = () => {
-	const diceType = dice.map(die => <Die key={die} die={die} />)
+  const [state, dispatch] = useReducer(diceReducer, initialState);
+	
+		const handleRoll = (type, value) => {
+			dispatch({state, type: 'SETDIE', die: type, value: value})
+
+		}
+
+	const diceSet = dice.map(die => {
+		const {type, sides, scenario} = die
+		return	(
+		<Die key={type} die={type} sides={sides} scenario={scenario} roll={handleRoll} />
+		)
+	})
+
+	
 
 	return (
 		<section className='dice-sheet'>
@@ -12,7 +38,7 @@ export const DicePage = () => {
 					<h3 className='heading'>Dice</h3>
 					<h3 className='heading'>Amount</h3>
 				</div>
-					{diceType}
+					{diceSet }
 			</div>
 		</section>
 	)
