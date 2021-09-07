d526091003d0ee965bfdaea493ec1c365c7508f2
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-07 00:19:20 -0600

    Remove Props

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -4,16 +4,14 @@ import { FormCharacteristics } from "./FormCharacteristics";
 import { FormCharDetails } from "./FormCharDetails";
 import { UserContext } from "../utilities/UserContext";
 
-export const FormContainer = ({ setCurrentChar }) => {
+export const FormContainer = () => {
   const [count, setCount] = useState(0)
   const { state: { currentChar } } = useContext(UserContext)
   const handleCount = () => setCount(count + 1)
 
-
-
   return (
     <section className="form-container">
-      {count === 0 && <FormCharDetails setCount={handleCount} setCharId={setCurrentChar} />}
+      {count === 0 && <FormCharDetails setCount={handleCount} />}
       {count === 1 && <FormCharacteristics charId={currentChar} setCount={handleCount} />}
       {count === 2 && <FormSkills charId={currentChar} setCount={handleCount} />}
     </section>
