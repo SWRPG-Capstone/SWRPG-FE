f1c1566abb2533bf18e152e7bdca52d50d6a6d11
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-07 16:38:52 -0600

    Refactor NavLink to be a versitile button

--- src/components/Orb.js
+++ src/components/Orb.js
@@ -1,19 +1,29 @@
 import React, { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory, useLocation } from 'react-router-dom';
+import { randomizeDice } from '../utilities/dice';
 import { UserContext } from '../utilities/UserContext'
 
 export const Orb = ({ pathway, icon, size }) => {
-  const { state: { isAuthorize } } = useContext(UserContext)
+  // const { state: { isAuthorize } } = useContext(UserContext)
   const file = icon.substr(14)
   const iconName = file.substr(0, file.indexOf('.'))
+  const history = useHistory();
+  const location = useLocation().pathname
+
+
+  const handleClick = () => {
+    console.log(location)
+    return location !== '/dice' ?
+    history.push(pathway) :
+    randomizeDice()
+  }
 
   return (
     <>
-      {/* Remove the !-bang operator to disable the hide feature for developement */}
-      <NavLink className={`button ${size} ${!isAuthorize && 'disabled'}`} to={pathway} >
+      <button className={`button ${size} '}`} onClick={handleClick} >
         <img className={`icon ${iconName} ${size}`} src={icon} alt={`${iconName} icon.`} />
         <div className={`orb ${size}`} />
-      </NavLink>
+      </button>
     </>
   )
 }
