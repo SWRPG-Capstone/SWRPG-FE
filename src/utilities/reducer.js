696611a671a8c287dbe2b3e998211210185f822b
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-08 09:53:31 -0600

    Add reducer to update state with roll outcome

--- src/utilities/reducer.js
+++ src/utilities/reducer.js
@@ -1,18 +1,24 @@
-export const reducer = (state, {action}) => {
+export const reducer = (state, { action }) => {
   switch (action.type) {
     case "SETCHARACTER":
-      const {character} = action
+      const { character } = action
       localStorage.setItem('token', character)
       return {
         ...state,
         currentChar: character
       }
-      case "SETDIE":
-        const {die, value} = action
-        return {
-          ...state,
-          [die]: value
-        }
+    case "SETDIE":
+      const { die, value } = action
+      return {
+        ...state,
+        [die]: value
+      }
+    case "SETOUTCOME":
+      const { results } = action
+      return {
+        ...state,
+        outcome: results
+      }
     case "AUTOSET":
       const token = JSON.parse(localStorage.getItem('token'))
       return token ?
