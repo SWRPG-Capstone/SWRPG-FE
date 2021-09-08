f12508d73ccef598fd52241213746deba9d42505
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-07 22:52:24 -0600

    Update dice roll logic

--- src/components/Orb.js
+++ src/components/Orb.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
+import { rollDice } from '../utilities/dice';
+import { UserContext } from '../utilities/UserContext';
+
 
 export const Orb = ({ pathway, icon, size }) => {
-
   const file = icon.substr(14)
   const iconName = file.substr(0, file.indexOf('.'))
   const history = useHistory();
   const location = useLocation().pathname
-
+  const { state } = useContext(UserContext)
 
   const handleClick = () => {
     return location === '/dice' && size === 'large' ?
-    console.log('location') :
-    history.push(pathway) 
+      rollDice(state) :
+      history.push(pathway)
   }
 
+
   return (
-    <>
-      <button className={`button ${size} '}`} onClick={handleClick} >
-        <img className={`icon ${iconName} ${size}`} src={icon} alt={`${iconName} icon.`} />
-        <div className={`orb ${size}`} />
-      </button>
-    </>
+    <button className={`button ${size} '}`} onClick={handleClick} >
+      <img className={`icon ${iconName} ${size}`} src={icon} alt={`${iconName} icon.`} />
+      <div className={`orb ${size}`} />
+    </button>
   )
 }
 
