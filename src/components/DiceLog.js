e81e7b610398112210f46c3fb3a31449b8e3adda
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-08 14:29:49 -0600

    Fix conditionals

--- src/components/DiceLog.js
+++ src/components/DiceLog.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react"
 
 export const DiceLog = ({ diceRolls }) => {
 	const [log, setLog] = useState([])
-	const { outcome} = diceRolls
+	const { outcome } = diceRolls
 	const diceSet = ['force', 'ability', 'proficiency', 'boost', 'difficulty', 'challenge', 'setback']
 
 	let countOutcome;
@@ -10,14 +10,13 @@ export const DiceLog = ({ diceRolls }) => {
 
 
 	useEffect(() => {
-		if (outcome) {
+		if ( outcome ) {
 			const translation = outcome.filter(entry => {
 				return entry.length
 			}).map((result) => {
 				const diceIndex = (outcome.indexOf(result))
 				const logEntry = countOutcome(result, diceIndex)
-				const newEntry = logEntry.props.children
-				log.length && !log < 1 ? setLog(newEntry) : setLog(...log, newEntry)
+				return !log.length && setLog(...log, logEntry)
 			})
 			return translation
 		}
@@ -37,13 +36,23 @@ export const DiceLog = ({ diceRolls }) => {
 			})
 			return acc
 		}, {})
-
-		
-
+		 
+		const getPhrases = Object.keys(totalResult).map(face => {
+			const total = Object.values(totalResult).map(amount => console.log(amount))
+			// const	total = Object.keys(totalResult).length
 			return (
-				<>{`You rolled ${dieAmount} ${die}`}</>		
-			)
-	};
+				<>{`You rolled ${dieAmount} ${die} dice and got ${total} ${face} `}</>		
+				)
+		})
+
+			const parsedPhrases = getPhrases.map(phrase => {
+				return phrase.props.children
+			})
+
+
+
+			return parsedPhrases
+		};
 
 	return (
 		<article className='roll-log'>
