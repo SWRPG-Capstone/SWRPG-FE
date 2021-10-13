f2d72eeaeb7d71e2f2b1a54739caed13bc426f39
Lauren Kessell <lkessell1@gmail.com>
2021-10-13 12:08:01 -0400

    Add tests for choosing number of dice to roll

--- cypress/integration/dice_page_spec.js
+++ cypress/integration/dice_page_spec.js
@@ -3,5 +3,22 @@ describe('Dice page user flows', () => {
     cy.visit('http://localhost:3000/dice');
   });
 
-  
+  it('Should show all types of dice', () => {
+    cy.get('article[class="die-field"]').should('have.length', 7);
+  });
+
+  it('Should be able to increase the number of dice rolled', () => {
+    cy.get('h4[class="dice-amount"]').first().contains('0');
+    cy.get('div[class="scale"]').first().click();
+    cy.get('div[class="scale"]').first().click();
+    cy.get('h4[class="dice-amount"]').first().contains('2');
+  });
+
+  it('Should be able to decrease the number of dice rolled', () => {
+    cy.get('div[class="scale"]').first().click();
+    cy.get('div[class="scale"]').first().click();
+    cy.get('h4[class="dice-amount"]').first().contains('2');
+    cy.get('div[class="scale down"]').first().click();
+    cy.get('h4[class="dice-amount"]').first().contains('1');
+  });
 })
