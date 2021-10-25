38a7a84d2c381d91780725ed2098779c07e57b52
Lauren Kessell <lkessell1@gmail.com>
2021-10-25 16:17:11 -0400

    Move form state, add new onChange

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,17 +1,41 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useReducer, useState } from "react";
 import { FormSkills } from "./FormSkills";
 import { FormCharacteristics } from "./FormCharacteristics";
 import { FormCharDetails } from "./FormCharDetails";
 import { UserContext } from "../utilities/UserContext";
+import { formReducer } from "../utilities/formReducer";
+
+const initialFormState = {
+  details: {
+    name: '',
+    species: '',
+    specialization: '',
+    career: '',
+    age: 0,
+    height: '',
+    build: '',
+    hair: '',
+    eyes: '',
+  }
+}
 
 export const FormContainer = () => {
   const [count, setCount] = useState(0)
   const { state: { currentChar } } = useContext(UserContext)
   const handleCount = () => setCount(count + 1)
+  const [formState, formDispatch] = useReducer(formReducer, initialFormState);
+
+  const onChange = (e, type) => {
+    formDispatch({
+      type: type,
+      field: e.target.name,
+      value: e.target.value,
+    });
+  }
 
   return (
     <section className="form-container">
-      {count === 0 && <FormCharDetails setCount={handleCount} />}
+      {count === 0 && <FormCharDetails setCount={handleCount} onChange={onChange} formState={formState} />}
       {count === 1 && <FormCharacteristics charId={currentChar} setCount={handleCount} />}
       {count === 2 && <FormSkills charId={currentChar} setCount={handleCount} />}
     </section>
