e225a1dad7aed4effa02430f2c55acc5a79ceb5e
Lauren Kessell <lkessell1@gmail.com>
2021-10-28 17:32:07 -0400

    Add skills to initial state, pass onChange and formState to skills form component

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -25,7 +25,41 @@ const initialFormState = {
     intellect: 1,
     willpower: 1
   },
-  skills: {}
+  skills: {
+    astrogation: 0,
+    athletics: 0,
+    brawl: 0,
+    charm: 0,
+    coercion: 0,
+    computers: 0,
+    cool: 0,
+    coordination: 0,
+    coreWorlds: 0,
+    deception: 0,
+    discipline: 0,
+    education: 0,
+    gunnery: 0,
+    leadership: 0,
+    lore: 0,
+    mechanics: 0,
+    medicine: 0,
+    melee: 0,
+    negotiation: 0,
+    outerRim: 0,
+    perception: 0,
+    piloting: 0,
+    pilotingSpace: 0,
+    rangedHeavy: 0,
+    rangedLight: 0,
+    resilience: 0,
+    skulduggery: 0,
+    stealth: 0,
+    streetWise: 0,
+    survival: 0,
+    underworld: 0,
+    vigilance: 0,
+    xenology: 0,
+  }
 }
 
 export const FormContainer = () => {
@@ -46,8 +80,8 @@ export const FormContainer = () => {
   return (
     <section className="form-container">
       {count === 0 && <FormCharDetails setCount={handleCount} onChange={onChange} formState={formState.details} />}
-      {count === 1 && <FormCharacteristics charId={currentChar} setCount={handleCount} onChange={onChange} formState={formState.characteristics} />}
-      {count === 2 && <FormSkills charId={currentChar} setCount={handleCount} />}
+      {count === 1 && <FormCharacteristics setCount={handleCount} onChange={onChange} formState={formState.characteristics} />}
+      {count === 2 && <FormSkills charId={currentChar} setCount={handleCount} onChange={onChange} formState={formState} />}
     </section>
   )
 }
