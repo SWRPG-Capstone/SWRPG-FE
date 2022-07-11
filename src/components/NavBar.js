6c7cfb59e4b01539b65f47acf745dd1c543c1f75
Lauren Kessell <lkessell1@gmail.com>
2022-07-11 16:50:30 -0400

    Use nav element in footer, update styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { Orb } from './Orb';
 import { icons } from '../utilities/icons';
-import { useLocation } from 'react-router';
 
 export const NavBar = () => {
   const { home, character, dice, skills, logout } = icons
-  const location = useLocation().pathname
 
   return (
-    <footer className={`${location === '/create' && 'disable'}`}>
-      <div className={`backdrop`} />
-      <section className={`nav-bar`}>
+    <footer>
+      <nav className={`nav-bar`}>
         <Orb pathway='/home' icon={home} size='small' />
         <Orb pathway='/character' icon={character} size='medium' />
         <Orb pathway='/dice' icon={dice} size='large' />
         <Orb pathway='/skills' icon={skills} size='medium' />
         <Orb pathway='/home' icon={logout} size='small' />
-      </section>
+      </nav>
+      <div className={`backdrop`} />
     </footer>
   )
 }
