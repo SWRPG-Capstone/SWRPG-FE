3e15dd3d5124a24075d71f27c087cbb9840b9f73
Lauren Kessell <lkessell1@gmail.com>
2022-07-12 13:29:55 -0400

    Add descriptive text to navigation buttons

--- src/components/Orb.js
+++ src/components/Orb.js
@@ -6,17 +6,17 @@ import { diceOutcome } from '../utilities/dice';
 
 
 export const Orb = ({ pathway, icon, size }) => {
-  const file = icon.substr(14)
-  const iconName = file.substr(0, file.indexOf('.'))
+  const file = icon.substr(14);
+  const iconName = file.substr(0, file.indexOf('.'));
   const history = useHistory();
-  const location = useLocation().pathname
-  const { state, dispatch } = useContext(UserContext)
+  const location = useLocation().pathname;
+  const { state, dispatch } = useContext(UserContext);
   
 
   const handleClick = () => {
     if (location === '/dice' && size === 'large') {
-      rollDice(state)
-      dispatch({state, action: {type: 'SETOUTCOME', results: diceOutcome}})
+      rollDice(state);
+      dispatch({state, action: {type: 'SETOUTCOME', results: diceOutcome}});
     } else {
       return history.push(pathway);
     }
@@ -24,8 +24,9 @@ export const Orb = ({ pathway, icon, size }) => {
 
 
   return (
-    <button className={`button ${size}`} onClick={handleClick} >
-      <img className={`icon ${iconName} ${size}`} src={icon} alt={`${iconName} icon.`} />
+    <button className={`button ${size}`} onClick={handleClick}>
+      <span className="sr-only">Go to {pathway.slice(1)} page</span>
+      <img aria-hidden className={`icon ${iconName} ${size}`} src={icon} alt={`${iconName} icon`} />
       <div className={`orb ${size}`} />
     </button>
   )
