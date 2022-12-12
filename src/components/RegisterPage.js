8eca64855b942444dd26ce77319e638322a94034
Lauren Kessell <lkessell1@gmail.com>
2022-12-12 15:29:31 -0500

    Add form state and username field

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const RegisterPage = () => {
+  const [formState, setFormState] = useState({ username: '', password: '', confirmPassword: '' });
+
+  const onChange = (e, field) => {
+    setFormState({ ...formState, [field]: e.target.value });
+  };
+
   return (
     <section>
-      <h2>Register a new Account</h2>
+      <h2>Register a new account</h2>
+      <form className="char-form">
+        <div className="input-container">
+          <label className="char-heading" htmlFor="username">
+            username
+            <input className="char-value" type="text" name="username" autoFocus value={formState.username} onChange={(e) => onChange(e, 'username')} />
+          </label>
+        </div>
+      </form>
     </section>
   );
 };
