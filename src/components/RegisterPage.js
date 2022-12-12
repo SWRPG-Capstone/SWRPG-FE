3235f50a453ceacf605d64424fefea9602832739
Lauren Kessell <lkessell1@gmail.com>
2022-12-12 15:36:53 -0500

    Add password and confirm password fields

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -17,6 +17,18 @@ export const RegisterPage = () => {
             <input className="char-value" type="text" name="username" autoFocus value={formState.username} onChange={(e) => onChange(e, 'username')} />
           </label>
         </div>
+        <div className="input-container">
+          <label className="char-heading" htmlFor="password">
+            password
+            <input className="char-value" type="text" name="password" value={formState.password} onChange={(e) => onChange(e, 'password')} />
+          </label>
+        </div>
+        <div className="input-container">
+          <label className="char-heading" htmlFor="confirmPassword">
+            confirm password
+            <input className="char-value" type="text" name="confirmPassword" value={formState.confirmPassword} onChange={(e) => onChange(e, 'confirmPassword')} />
+          </label>
+        </div>
       </form>
     </section>
   );
