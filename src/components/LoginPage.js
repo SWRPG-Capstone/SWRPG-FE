45f76e2c67d4f6b9f39efbb4b137bea5e05590c7
Lauren Kessell <lkessell1@gmail.com>
2023-01-03 14:48:05 -0500

    Set up username field

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const LoginPage = () => {
+  const [formState, setFormState] = useState({ username: '' });
+
+  const onChange = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
   return (
     <section>
       <h2>Log In</h2>
+      <form>
+        <div className="input-container">
+          <label className="char-heading" htmlFor="username">
+            username
+            <input className="char-value" type="text" name="username" autoFocus value={formState.username} onChange={onChange} />
+          </label>
+        </div>
+      </form>
     </section>
   );
 };
