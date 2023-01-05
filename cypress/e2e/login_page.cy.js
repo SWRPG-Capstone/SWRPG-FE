91f93ad49e06f5fab739cb35d3f8bd33465a3698
Lauren Kessell <lkessell1@gmail.com>
2023-01-05 12:55:02 -0500

    Add test for username error message

--- cypress/e2e/login_page.cy.js
+++ cypress/e2e/login_page.cy.js
@@ -4,4 +4,11 @@ describe('Log in user flows', () => {
     cy.get('.title').contains('log in').should('be.visible');
     cy.url().should('include', '/login');
   });
+
+  it('Should display an error message after clicking the submit button if a username is not provided', () => {
+    cy.visit('http://localhost:3000/login');
+    cy.get('.inline-error').should('not.exist');
+    cy.get('button').contains('Submit').click();
+    cy.get('.inline-error').contains('Please enter a username').should('be.visible');
+  });
 });
