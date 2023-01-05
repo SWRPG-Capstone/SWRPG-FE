06a21a51598107cddb0962bd4ad494cf8e610774
Lauren Kessell <lkessell1@gmail.com>
2023-01-05 12:56:36 -0500

    Add test for password error message

--- cypress/e2e/login_page.cy.js
+++ cypress/e2e/login_page.cy.js
@@ -11,4 +11,12 @@ describe('Log in user flows', () => {
     cy.get('button').contains('Submit').click();
     cy.get('.inline-error').contains('Please enter a username').should('be.visible');
   });
+
+  it('Should display an error message after clicking the submit button if a password is not provided', () => {
+    cy.visit('http://localhost:3000/login');
+    cy.get('.inline-error').should('not.exist');
+    cy.get('input[name="username"]').type('CoolMcCool');
+    cy.get('button').contains('Submit').click();
+    cy.get('.inline-error').contains('Please enter a password').should('be.visible');
+  });
 });
