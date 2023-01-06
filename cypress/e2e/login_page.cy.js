d116b9728fc4dfab58221d386e4fc5b8251c1e60
Lauren Kessell <lkessell1@gmail.com>
2023-01-06 11:27:57 -0500

    Add test for invalid login error

--- cypress/e2e/login_page.cy.js
+++ cypress/e2e/login_page.cy.js
@@ -26,4 +26,24 @@ describe('Log in user flows', () => {
     cy.get('button').contains('Submit').click();
     cy.get('.inline-error').contains('Please enter a password').should('be.visible');
   });
+
+  it('Should display an error message if invalid login credentials are submitted', () => {
+    cy.intercept('POST', 'https://rails-2swo.onrender.com/graphql', (req) => {
+      if (req.body.operationName === 'loginUser') {
+        req.alias = 'loginUserMutation';
+        req.reply({
+          data: {
+            loginUser: null,
+          },
+        });
+      }
+    });
+
+    cy.visit('http://localhost:3000/login');
+    cy.get('input[name="username"]').type('CoolMcCool');
+    cy.get('input[name="password"]').type('Wrong123&');
+    cy.get('.login-error').should('not.exist');
+    cy.get('button').contains('Submit').click();
+    cy.get('.login-error').contains('Incorrect username or password.').should('be.visible');
+  });
 });
