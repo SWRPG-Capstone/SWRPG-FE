c00b48063288b791c360f6cef759242699b046bd
Lauren Kessell <lkessell1@gmail.com>
2023-01-09 18:35:45 -0500

    Refactor RouteGuard

--- src/components/RouteGuard.js
+++ src/components/RouteGuard.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-export const RouteGuard = ({ token, component: Component, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={(props) => (token ? <Component {...props} /> : <Redirect to={{ pathname: '/login' }} />)}
-    />
-  );
+export const RouteGuard = ({ token, children, ...rest }) => {
+  return token ? <Route {...rest}>{children}</Route> : <Redirect to="/login" />;
 };
