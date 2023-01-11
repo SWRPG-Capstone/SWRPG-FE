07089a041b58659c791373e801ee9603ecba100a
Lauren Kessell <lkessell1@gmail.com>
2023-01-11 12:46:45 -0500

    Add SETUSER case to reducer

--- src/utilities/reducer.js
+++ src/utilities/reducer.js
@@ -1,33 +1,39 @@
 export const reducer = (state, { action }) => {
   switch (action.type) {
-    case "SETCHARACTER":
-      const { character } = action
-      localStorage.setItem('token', character)
+    case 'SETCHARACTER':
+      const { character } = action;
+      localStorage.setItem('token', character);
       return {
         ...state,
-        currentChar: character
-      }
-    case "SETDIE":
-      const { die, value } = action
+        currentChar: character,
+      };
+    case 'SETDIE':
+      const { die, value } = action;
       return {
         ...state,
-        [die]: value
-      }
-    case "SETOUTCOME":
-      const { results } = action
+        [die]: value,
+      };
+    case 'SETOUTCOME':
+      const { results } = action;
       return {
         ...state,
-        outcome: results
-      }
-    case "AUTOSET":
-      const token = JSON.parse(localStorage.getItem('token'))
-      return token ?
-        {
-          ...state,
-          currentChar: token,
-        } : state
+        outcome: results,
+      };
+    case 'SETUSER':
+      const { userID } = action;
+      return {
+        ...state,
+        currentUser: userID,
+      };
+    case 'AUTOSET':
+      const token = JSON.parse(localStorage.getItem('token'));
+      return token
+        ? {
+            ...state,
+            currentChar: token,
+          }
+        : state;
     default:
-      return state
+      return state;
   }
 };
-
