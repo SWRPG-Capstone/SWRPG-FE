b09e4c1388cbd9e8d48e9dc4a5c6a763a3b2c0b8
Lauren Kessell <lkessell1@gmail.com>
2023-01-15 12:36:03 -0500

    Add cypress login command

---
+++ cypress/support/commands.js
@@ -0,0 +1,23 @@
+Cypress.Commands.add('login', (username, password) => {
+  cy.intercept('POST', 'https://rails-2swo.onrender.com/graphql', (req) => {
+    if (req.body.operationName === 'loginUser') {
+      req.alias = 'loginUserMutation';
+      req.reply({
+        data: {
+          loginUser: {
+            token: 'testToken126178',
+            user: {
+              id: '1',
+            },
+          },
+        },
+      });
+    }
+  });
+
+  cy.visit('/login');
+  cy.get('input[name="username"]').type(username);
+  cy.get('input[name="password"]').type(password);
+  cy.get('button').contains('Submit').click();
+  cy.get('.title').contains('home').should('be.visible');
+});
