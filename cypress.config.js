706ea4854f868000737a015a4b4f22519ebb7e93
Lauren Kessell <lkessell1@gmail.com>
2023-01-19 15:54:32 -0500

    Remove deprecated option

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config);
     },
