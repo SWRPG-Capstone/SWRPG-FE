b13078a94983dc2c507e8b95402c02bcabc3628f
Lauren Kessell <lkessell1@gmail.com>
2023-03-02 16:35:11 -0500

    Render characteristics form fields with FormField component

--- src/components/FormCharacteristics.js
+++ src/components/FormCharacteristics.js
@@ -24,8 +24,11 @@ export const FormCharacteristics = ({ setCount, onChange, formState }) => {
     return (
       <FormField
         key={name}
+        label={name === 'charPresence' ? 'presence' : name}
         name={name}
         type="number"
+        min="1"
+        max="5"
         value={formState[name]}
         onChange={(e) => onChange(e, 'handle number input', 'characteristics')}
         autoFocus={name === 'agility' ? true : false}
@@ -35,96 +38,7 @@ export const FormCharacteristics = ({ setCount, onChange, formState }) => {
 
   return (
     <form className="characteristic-form" autoComplete="on">
-      <div className="input-container">
-        <label className="char-heading" htmlFor="agility">
-          agility
-          <input
-            className="char-value"
-            type="number"
-            min="1"
-            max="5"
-            name="agility"
-            value={formState.agility}
-            onChange={(e) => onChange(e, 'handle number input', 'characteristics')}
-            autoFocus
-          />
-        </label>
-      </div>
-
-      <div className="input-container">
-        <label className="char-heading" htmlFor="brawn">
-          brawn
-          <input
-            className="char-value"
-            type="number"
-            min="1"
-            max="5"
-            name="brawn"
-            value={formState.brawn}
-            onChange={(e) => onChange(e, 'handle number input', 'characteristics')}
-          />
-        </label>
-      </div>
-
-      <div className="input-container">
-        <label className="char-heading" htmlFor="charPresence">
-          presence
-          <input
-            className="char-value"
-            type="number"
-            min="1"
-            max="5"
-            name="charPresence"
-            value={formState.charPresence}
-            onChange={(e) => onChange(e, 'handle number input', 'characteristics')}
-          />
-        </label>
-      </div>
-
-      <div className="input-container">
-        <label className="char-heading" htmlFor="cunning">
-          cunning
-          <input
-            className="char-value"
-            type="number"
-            min="1"
-            max="5"
-            name="cunning"
-            value={formState.cunning}
-            onChange={(e) => onChange(e, 'handle number input', 'characteristics')}
-          />
-        </label>
-      </div>
-
-      <div className="input-container">
-        <label className="char-heading" htmlFor="intellect">
-          intellect
-          <input
-            className="char-value"
-            type="number"
-            min="1"
-            max="5"
-            name="intellect"
-            value={formState.intellect}
-            onChange={(e) => onChange(e, 'handle number input', 'characteristics')}
-          />
-        </label>
-      </div>
-
-      <div className="input-container">
-        <label className="char-heading" htmlFor="willpower">
-          willpower
-          <input
-            className="char-value"
-            type="number"
-            min="1"
-            max="5"
-            name="willpower"
-            value={formState.willpower}
-            onChange={(e) => onChange(e, 'handle number input', 'characteristics')}
-          />
-        </label>
-      </div>
+      {formFields}
 
       {validated === false && <p className="form-error-msg">Characteristics must have a value between 1 and 5</p>}
 
