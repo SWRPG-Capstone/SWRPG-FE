a3b1229b49882ec7f5f051eb153d7e6439d40c73
Lauren Kessell <lkessell1@gmail.com>
2023-03-02 15:47:42 -0500

    Add min and max fields

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,9 +1,18 @@
-export const FormField = ({ name, type, value, onChange, autoFocus }) => {
+export const FormField = ({ name, type, value, onChange, autoFocus, min, max }) => {
   return (
     <div className="input-container">
       <label className="char-heading" htmlFor={name}>
         {name}
-        <input className="char-value" type={type} name={name} value={value} onChange={onChange} autoFocus={autoFocus} />
+        <input
+          className="char-value"
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+          autoFocus={autoFocus}
+          min={min}
+          max={max}
+        />
       </label>
     </div>
   );
