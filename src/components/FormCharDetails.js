96e3e6608d9faa0b496bb63bdd02a3089da046ec
Lauren Kessell <lkessell1@gmail.com>
2023-03-02 14:44:26 -0500

    Add variable for all details form field names

--- src/components/FormCharDetails.js
+++ src/components/FormCharDetails.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { FormField } from './FormField';
 
 export const FormCharDetails = ({ setCount, onChange, formState }) => {
   const [validated, setValidated] = useState(null);
+  const fieldNames = Object.keys(formState).filter((key) => key !== 'userID');
 
   const validateForm = () => {
-    return Object.keys(formState).reduce((valid, field) => {
+    return fieldNames.reduce((valid, field) => {
       if (field === 'age') {
         if (formState.age <= 0) valid = false;
       } else {
@@ -12,7 +14,7 @@ export const FormCharDetails = ({ setCount, onChange, formState }) => {
       }
       return valid;
     }, true);
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,66 +23,122 @@ export const FormCharDetails = ({ setCount, onChange, formState }) => {
     if (formComplete) {
       setCount();
     }
-  }
+  };
 
   return (
-    <form className='char-form' onSubmit={handleSubmit} >
-      <div className='input-container'>
+    <form className="char-form" onSubmit={handleSubmit}>
+      {/* <div className='input-container'>
         <label className='char-heading' htmlFor="name">
           name
           <input className='char-value' type="text" name="name" value={formState.name} onChange={(e) => onChange(e, 'handle text input', 'details')} autoFocus />
         </label>
-      </div>
-      <div className='input-container'>
-        <label className='char-heading' htmlFor="species">
+      </div> */}
+      <FormField
+        name="name"
+        type="text"
+        value={formState.name}
+        onChange={(e) => onChange(e, 'handle text input', 'details')}
+      />
+      <div className="input-container">
+        <label className="char-heading" htmlFor="species">
           species
-          <input className='char-value' type="text" name="species" value={formState.species} onChange={(e) => onChange(e, 'handle text input', 'details')} />
+          <input
+            className="char-value"
+            type="text"
+            name="species"
+            value={formState.species}
+            onChange={(e) => onChange(e, 'handle text input', 'details')}
+          />
         </label>
       </div>
-      <div className='input-container'>
-        <label className='char-heading' htmlFor="specialization">
+      <div className="input-container">
+        <label className="char-heading" htmlFor="specialization">
           specialization
-          <input className='char-value' type="text" name="specialization" value={formState.specialization} onChange={(e) => onChange(e, 'handle text input', 'details')} />
+          <input
+            className="char-value"
+            type="text"
+            name="specialization"
+            value={formState.specialization}
+            onChange={(e) => onChange(e, 'handle text input', 'details')}
+          />
         </label>
       </div>
-      <div className='input-container'>
-        <label className='char-heading' htmlFor="career">
+      <div className="input-container">
+        <label className="char-heading" htmlFor="career">
           career
-          <input className='char-value' type="text" name="career" value={formState.career} onChange={(e) => onChange(e, 'handle text input', 'details')} />
+          <input
+            className="char-value"
+            type="text"
+            name="career"
+            value={formState.career}
+            onChange={(e) => onChange(e, 'handle text input', 'details')}
+          />
         </label>
       </div>
-      <div className='input-container'>
-        <label className='char-heading' htmlFor="age">
+      <div className="input-container">
+        <label className="char-heading" htmlFor="age">
           age
-          <input className='char-value' type="number" name="age" value={formState.age} onChange={(e) => onChange(e, 'handle number input', 'details')} />
+          <input
+            className="char-value"
+            type="number"
+            name="age"
+            value={formState.age}
+            onChange={(e) => onChange(e, 'handle number input', 'details')}
+          />
         </label>
       </div>
-      <div className='input-container'>
-        <label className='char-heading' htmlFor="height">
+      <div className="input-container">
+        <label className="char-heading" htmlFor="height">
           height
-          <input className='char-value' type="text" name="height" value={formState.height} onChange={(e) => onChange(e, 'handle text input', 'details')} />
+          <input
+            className="char-value"
+            type="text"
+            name="height"
+            value={formState.height}
+            onChange={(e) => onChange(e, 'handle text input', 'details')}
+          />
         </label>
       </div>
-      <div className='input-container'>
-        <label className='char-heading' htmlFor="build">
+      <div className="input-container">
+        <label className="char-heading" htmlFor="build">
           build
-          <input className='char-value' type="text" name="build" value={formState.build} onChange={(e) => onChange(e, 'handle text input', 'details')} />
+          <input
+            className="char-value"
+            type="text"
+            name="build"
+            value={formState.build}
+            onChange={(e) => onChange(e, 'handle text input', 'details')}
+          />
         </label>
       </div>
-      <div className='input-container'>
-        <label className='char-heading' htmlFor="hair">
+      <div className="input-container">
+        <label className="char-heading" htmlFor="hair">
           hair
-          <input className='char-value' type="text" name="hair" value={formState.hair} onChange={(e) => onChange(e, 'handle text input', 'details')} />
+          <input
+            className="char-value"
+            type="text"
+            name="hair"
+            value={formState.hair}
+            onChange={(e) => onChange(e, 'handle text input', 'details')}
+          />
         </label>
       </div>
-      <div className='input-container'>
-        <label className='char-heading' htmlFor="eyes">
+      <div className="input-container">
+        <label className="char-heading" htmlFor="eyes">
           eyes
-          <input className='char-value' type="text" name="eyes" value={formState.eyes} onChange={(e) => onChange(e, 'handle text input', 'details')} />
+          <input
+            className="char-value"
+            type="text"
+            name="eyes"
+            value={formState.eyes}
+            onChange={(e) => onChange(e, 'handle text input', 'details')}
+          />
         </label>
       </div>
-      {validated === false && <p className='form-error-msg'>Please fill out all fields to continue</p>}
-      <button className='button large' type='submit'>Next</button>
+      {validated === false && <p className="form-error-msg">Please fill out all fields to continue</p>}
+      <button className="button large" type="submit">
+        Next
+      </button>
     </form>
-  )
-}
+  );
+};
