fe57dc7f75f21f48d78e5cd1acd2be2e694f542a
Lauren Kessell <lkessell1@gmail.com>
2023-03-02 15:21:54 -0500

    Add autoFocus prop to FormField

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,9 +1,9 @@
-export const FormField = ({ name, type, value, onChange }) => {
+export const FormField = ({ name, type, value, onChange, autoFocus }) => {
   return (
     <div className="input-container">
       <label className="char-heading" htmlFor={name}>
         {name}
-        <input className="char-value" type={type} name={name} value={value} onChange={onChange} />
+        <input className="char-value" type={type} name={name} value={value} onChange={onChange} autoFocus={autoFocus} />
       </label>
     </div>
   );
