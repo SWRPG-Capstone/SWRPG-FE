15b16ee7fe842e9f71d3017bfffb9501d453d986
Lauren Kessell <lkessell1@gmail.com>
2023-05-11 18:07:24 -0400

    Add ref to dialog, close/open modal based on isOpen prop

--- src/components/DialogModal.js
+++ src/components/DialogModal.js
@@ -1,6 +1,18 @@
+import { useEffect, useRef } from 'react';
+
 export const DialogModal = ({ isOpen, closeModal, children }) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      ref.current?.showModal();
+    } else {
+      ref.current?.close();
+    }
+  }, [isOpen]);
+
   return (
-    <dialog>
+    <dialog ref={ref} onClick={closeModal}>
       <div>{children}</div>
       <button onClick={closeModal}>Close</button>
     </dialog>
